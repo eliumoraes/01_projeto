4e7bfcfa549c9ea2e76ee1e9ec3a7cc8e3600751
Eliu Moraes <eliumoraes@gmail.com>
2020-05-04 16:26:05 -0300

    Continuando edições do projeto inicial

--- canivete_project/settings.py
+++ canivete_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard.apps.DashboardConfig',
+    #'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
