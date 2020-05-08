8925623eb05268f76982489b31fc7abd7c68c1c7
Adriana Guimarães <adrianacgennari@icloud.com>
2020-05-07 22:31:42 -0300

    Foi criada view Dri | Foi iniciado o DashBoard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, DricaPageView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('dri/', DricaPageView.as_view(), name='dri'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class DricaPageView(TemplateView):
+    template_name = 'dri.html'
